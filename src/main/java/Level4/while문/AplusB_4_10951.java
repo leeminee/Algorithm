108f58712ce881d87d1de282accc54da080424bd
Minee <mingramer@gmail.com>
2021-01-04 07:47:32 +0900

    백준 알고리즘 Level 4도전중

--- src/main/java/Level4/while문/AplusB_4_10951.java
+++ src/main/java/Level4/while문/AplusB_4_10951.java
@@ -1,4 +1,15 @@
 package Level4.while문;
 
+import java.util.Scanner;
+
 public class AplusB_4_10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+
+    }
 }
