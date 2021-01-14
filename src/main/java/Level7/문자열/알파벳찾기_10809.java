cdb3f1406fb0b137dc6df7a4bd0901d98024b849
À̹ÎÀÌ <mllee@ipageon.com>
2021-01-15 00:26:15 +0900

    문자열 알파벳찾기 문제 1개품

--- src/main/java/Level7/문자열/알파벳찾기_10809.java
+++ src/main/java/Level7/문자열/알파벳찾기_10809.java
@@ -1,4 +1,31 @@
 package Level7.문자열;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 알파벳찾기_10809 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        String word = br.readLine();
+        br.close();
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if(arr[ch-'a']==-1){
+                arr[ch-'a'] = i;
+            }
+        }
+
+        for(int val : arr){
+            System.out.print(val+" ");
+        }
+    }
 }
