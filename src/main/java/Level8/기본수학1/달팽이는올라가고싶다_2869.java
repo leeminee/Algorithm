e0b0c7481a860d32696b0a46f3cf779cb23c8b5b
Minee <mingramer@gmail.com>
2021-01-19 22:14:20 +0900

    백준 알고리즘 Level 8 달팽이는 올라가고싶다 1개품

--- src/main/java/Level8/기본수학1/달팽이는올라가고싶다_2869.java
+++ src/main/java/Level8/기본수학1/달팽이는올라가고싶다_2869.java
@@ -1,4 +1,24 @@
 package Level8.기본수학1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 달팽이는올라가고싶다_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        br.close();
+        int day = (V-B)/(A-B);
+        if((V-B)%(A-B) == 0){
+            System.out.println(day);
+        } else {
+            System.out.println(++day);
+        }
+    }
 }
--- src/main/java/Level8/기본수학1/분수찾기_1193.java
+++ src/main/java/Level8/기본수학1/분수찾기_1193.java
@@ -1,4 +1,13 @@
 package Level8.기본수학1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 분수찾기_1193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
 }
