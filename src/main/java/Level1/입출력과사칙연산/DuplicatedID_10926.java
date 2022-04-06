5b8e05fc75015992a8f9791e1f73af653554a192
이민이 <mllee@ipageon.com>
2022-04-06 16:57:17 +0900

    백준알고리즘 문제풀기 다시시작!

---
+++ src/main/java/Level1/입출력과사칙연산/DuplicatedID_10926.java
@@ -0,0 +1,11 @@
+package Level1.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class DuplicatedID_10926 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String id = sc.next();
+    System.out.println(id+"??!");
+  }
+}
---
+++ src/main/java/Level1/입출력과사칙연산/태국에서_18108.java
@@ -0,0 +1,11 @@
+package Level1.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 태국에서_18108 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int year = sc.nextInt();
+    System.out.println(year-543);
+  }
+}
